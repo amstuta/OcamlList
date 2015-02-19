9219d92db1c7456811e801b34d20819035d55a26
amstutz <arthur.amstutz@gmail.com>
2015-02-19 16:03:10 +0100

    add hd

--- my_list.ml
+++ my_list.ml
@@ -4,16 +4,29 @@ type 'a my_list =
 ;;
 
 
-(* List length *)
+(* List.length *)
 let rec length my_list =
 	match my_list with
 	| Empty -> 0
        	| Item (hd, tl) -> 1 + length (tl)
 ;;
 
+(* List.hd *)
+let rec hd my_list =
+	match my_list with
+	| Empty -> 0 (* throw exception *)
+	| Item (hd, tl) -> hd
 
-let a = Item("salope", Item("encule", Item("biatch", Empty)));;
+let a = Item(1, Item( 2, Item(3, Empty)));;
+let e = Item("a", Item("b", Item("c", Empty)));;
+
 let b = length a;;
-
 print_int b;;
 print_endline "";;
+
+
+let c = hd a;;
+print_int c;;
+print_endline "";;
+let d = hd e;;
+print_endline d;;
